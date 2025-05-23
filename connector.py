9e94d52d882b21fcea0f9af9824962a4e90fdc86
Georges Janin <georges.janin@gmail.com>
2025-05-23 15:15:24 +0200

    Initial commit: add connector and XML files

---
+++ connector.py
@@ -0,0 +1,62 @@
+import requests
+from dicttoxml import dicttoxml
+from github import Github
+import os
+
+# adding connection to the .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+
+url = 'https://active-jobs-db.p.rapidapi.com/active-ats-7d'
+querystring = {
+    "limit": "10",
+    "offset": "0",
+    "title_filter": '"Data Engineer"',
+    "location_filter": '"United States" OR "United Kingdom"',
+    "description_type": "text"
+}
+headers = {
+    'x-rapidapi-host': 'active-jobs-db.p.rapidapi.com',
+    'x-rapidapi-key': os.environ.get('RAPID_API_KEY') 
+}
+
+
+response = requests.get(url, headers=headers, params=querystring)
+response.raise_for_status()
+json_data = response.json()
+
+
+xml_data = dicttoxml(json_data, custom_root='jobs', attr_type=False)
+
+
+xml_file_path = "job_data.xml"
+with open(xml_file_path, "wb") as f:
+    f.write(xml_data)
+print("XML conversion done and saved locally.")
+
+
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+REPO_NAME = "GeorgesJanin/xml_hosting"  
+FILE_PATH_IN_REPO = "job_data.xml"      
+COMMIT_MESSAGE = "Update job data XML"
+
+g = Github(GITHUB_TOKEN)
+repo = g.get_repo(REPO_NAME)
+
+try:
+    contents = repo.get_contents(FILE_PATH_IN_REPO)
+    repo.update_file(
+        path=contents.path,
+        message=COMMIT_MESSAGE,
+        content=xml_data.decode('utf-8'),
+        sha=contents.sha
+    )
+    print("File updated in GitHub repo.")
+except Exception as e:
+    repo.create_file(
+        path=FILE_PATH_IN_REPO,
+        message=COMMIT_MESSAGE,
+        content=xml_data.decode('utf-8')
+    )
+    print("File created in GitHub repo.")
